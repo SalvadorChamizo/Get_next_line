dcbe344dff94fd1dbcc27eb51bcbfd5b6da209b3
SalvadorChamizo <139229383+SalvadorChamizo@users.noreply.github.com>
2024-03-11 11:12:31 +0100

    Uploading files
    Files from /bns and /src

---
+++ bns/get_next_line_bonus.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schamizo <schamizo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/22 11:02:41 by schamizo          #+#    #+#             */
+/*   Updated: 2023/12/22 11:08:53 by schamizo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <ctype.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_memset(void *b, int c, size_t len);
+void	safe_free(char *str);
+
+#endif
---
+++ bns/get_next_line_utils_bonus.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schamizo <schamizo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/22 11:03:16 by schamizo          #+#    #+#             */
+/*   Updated: 2023/12/22 11:03:26 by schamizo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*p;
+
+	p = malloc(count * size);
+	if (p == NULL)
+		return (NULL);
+	ft_memset(p, 0, count * size);
+	return (p);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*s3;
+	char	*p;
+	size_t	len1;
+	size_t	len2;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	s3 = malloc(len1 + len2 + 1);
+	if (s3 == NULL)
+		return (NULL);
+	p = s3;
+	while (len1--)
+		*p++ = *s1++;
+	while (len2--)
+		*p++ = *s2++;
+	*p = '\0';
+	return (s3);
+}
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*p;
+
+	p = b;
+	if (len == 0)
+		return (b);
+	while (len > 0)
+	{
+		*p = (unsigned char) c;
+		p++;
+		len--;
+	}
+	return (b);
+}
+
+void	safe_free(char *str)
+{
+	free(str);
+	str = NULL;
+}
